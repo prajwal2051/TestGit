8d65a616e1b2999a23178344d33b0d1188e48423
prajwal2051 <prajwalbhandari2051@gmail.com>
2020-02-05 18:14:18 -0600

    2nd File

---
+++ Email_id.java
@@ -0,0 +1,39 @@
+package University;
+
+public class Email_id {
+	protected static int Age;
+	public String Class;
+	private int Grade;
+
+	/*Addition obj = new Addition();
+    /* It will throw error because we are trying to access
+     * the default method in another package
+     
+obj.addTwoNumbers(10, 21);
+}
+	*/
+	
+	public static void main(String[] args) {
+	    Email_id object = new Email_id();
+	    return Age;
+	  }
+}
+/*protected int getAge()
+{
+	return Age;
+}
+	
+
+/*public String getClass()
+{
+	return Class;
+	}
+
+private int getGrade()
+{
+	return Grade;
+}
+}
+
+          
+*/
---
+++ My JAVA Files/MainProgram.java
@@ -0,0 +1,20 @@
+package University;
+
+public class MainProgram {
+
+	
+	
+	public static void main(String[] args) {
+		Calculator calculate = new Calculator();
+		
+		
+		calculate.Addition(12,8);//small a
+		
+		calculate.Subtraction(12,8);
+		
+		calculate.PrintSmaller(12,8);
+
+	}
+
+}
+
